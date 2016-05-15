3e68c02ced7f14409339cea06aa74c5c5815aa39
ShevchenkoPO <ofpana@mail.ru>
2016-05-16 01:39:58 +0600

    +

--- FormatJavaCode_v2/src/com/company/Formatcode/IFormater.java
+++ FormatJavaCode_v2/src/com/company/Formatcode/IFormater.java
@@ -2,20 +2,15 @@ package com.company.Formatcode;
 
 public class IFormater {
 
-    static int IFormat(int simb){
-        if ((simb == 59 ) || (simb == 125))
-        {
+    static int IFormat(int simb) {
+        if ((simb == 59 ) || (simb == 125)) {
             System.out.print("\n");
             return 0;
         }
-
-        if (simb == 123)
-        {
+        if (simb == 123) {
             System.out.print("\n    ");
             return 0;
         }
-
         return simb;
     }
-
 }
